f94c0f2fa2066a308efb53c7a636edd304ffbaf3
user <yannlefranc330@gmail.com>
2023-09-23 15:54:07 +0200

    Merci

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        //declaration des variables
+        int twerk1 =  0 + 1  ;
+        int twerk2 = twerk1 + 1 ;
+        int twerk3 = twerk1 + twerk2;
+        String chaz = "Merci";
+
+        System.out.println(twerk1);
+
+        System.out.println(twerk2);
+
+        System.out.println(twerk3);
+
+        System.out.println(chaz);
+
+
+
+
+
+        }
+    }
